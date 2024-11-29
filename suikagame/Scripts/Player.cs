6b88e5114513320d1ab1c6e66d3cd90c17e81406
CiaranMcIlvaney <000945633@mohawkcollege.ca>
2024-11-28 22:42:40 -0500

    Create each fruit

--- suikagame/Scripts/Player.cs
+++ suikagame/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Player : Node2D
 {
@@ -7,8 +8,30 @@ public partial class Player : Node2D
     private float speed = 200f;
     private float direction = 1f; 
     private float timer = 0f; 
-    private float timeLimit = 2.5f; 
+    private float timeLimit = 2.5f;
 
+    [Export]
+    private PackedScene Cherry;
+    [Export]
+    private PackedScene Strawberry;
+    [Export]
+    private PackedScene Grape;
+    [Export]
+    private PackedScene Dekopon;
+    [Export]
+    private PackedScene Persimmon;
+    [Export]
+    private PackedScene Apple;
+    [Export]
+    private PackedScene Pear;
+    [Export]
+    private PackedScene Peach;
+    [Export]
+    private PackedScene Pineapple;
+    [Export]
+    private PackedScene Melon;
+    [Export]
+    private PackedScene TitularWatermelon;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -30,6 +53,42 @@ public partial class Player : Node2D
         {
             direction *= -1; // Reverse direction
             timer = 0f; // Reset the timer
+        }
+
+        if (Input.IsActionJustPressed("drop"))
+        {
+            Node2D cherry = Cherry.Instantiate<Node2D>();
+            this.AddChild(cherry);
+
+            Node2D strawberry = Strawberry.Instantiate<Node2D>();
+            this.AddChild(strawberry);
+
+            Node2D grape = Grape.Instantiate<Node2D>();
+            this.AddChild(grape);
+
+            Node2D dekopon = Dekopon.Instantiate<Node2D>();
+            this.AddChild(dekopon);
+
+            Node2D persimmon = Persimmon.Instantiate<Node2D>();
+            this.AddChild(persimmon);
+
+            Node2D apple = Apple.Instantiate<Node2D>();
+            this.AddChild(apple);
+
+            Node2D pear = Pear.Instantiate<Node2D>();
+            this.AddChild(pear);
+
+            Node2D peach = Peach.Instantiate<Node2D>();
+            this.AddChild(peach);
+
+            Node2D pineapple = Pineapple.Instantiate<Node2D>();
+            this.AddChild(pineapple);
+
+            Node2D melon = Melon.Instantiate<Node2D>();
+            this.AddChild(melon);
+
+            Node2D titularWatermelon = TitularWatermelon.Instantiate<Node2D>();
+            this.AddChild(titularWatermelon);
         }
 
     }
